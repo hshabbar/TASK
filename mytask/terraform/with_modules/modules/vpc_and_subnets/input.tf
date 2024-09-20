014fa73bbf226d7ffdd6e4b69e86bd47fa2aabd0
hshabbar <91328430+hshabbar@users.noreply.github.com>
2024-09-20 22:55:32 +0530

    Create input.tf

---
+++ mytask/terraform/with_modules/modules/vpc_and_subnets/input.tf
@@ -0,0 +1,53 @@
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type = string
+  default = "172.32.0.0/16"
+}
+
+variable "public_subnet_one_cidr" {
+  description = "CIDR for public subnet one"
+  type = string
+  default = "172.32.0.0/20"
+}
+
+variable "public_subnet_two_cidr" {
+  description = "CIDR for public subnet two"
+  type = string
+  default = "172.32.16.0/20"
+}
+
+variable "public_subnet_one_availability_zone" {
+  description = "availability zone for public subnet one"
+  type = string
+  default = "ap-south-1a"
+}
+
+variable "public_subnet_two_availability_zone" {
+  description = "availability zone for public subnet two"
+  type = string
+  default = "ap-south-1b"
+}
+
+variable "private_subnet_one_cidr" {
+  description = "CIDR for private subnet one"
+  type = string
+  default = "172.32.32.0/20"
+}
+
+variable "private_subnet_two_cidr" {
+  description = "CIDR for private subnet two"
+  type = string
+  default = "172.32.64.0/20"
+}
+
+variable "private_subnet_one_availability_zone" {
+  description = "availability zone for private subnet one"
+  type = string
+  default = "ap-south-1a"
+}
+
+variable "private_subnet_two_availability_zone" {
+  description = "availability zone for private subnet two"
+  type = string
+  default = "ap-south-1b"
+}
