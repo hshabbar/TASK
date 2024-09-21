8e2e7fe7fa27dfd6649409477564c8ead4bf1f2a
hshabbar <91328430+hshabbar@users.noreply.github.com>
2024-09-21 10:13:32 +0530

    Update eks.tf

--- mytask/terraform/with_modules/modules/eks/eks.tf
+++ mytask/terraform/with_modules/modules/eks/eks.tf
@@ -1,83 +1,83 @@
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role_terraform"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-
-resource "aws_iam_role_policy_attachment" "my-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-
-resource "aws_iam_role_policy_attachment" "my-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-
-resource "aws_eks_cluster" "my_eks_cluster" {
-  name     = var.eks_cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  vpc_config { 
-    subnet_ids = [var.subnet_one, var.subnet_two]
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.my-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.my-AmazonEKSVPCResourceController,
-  ]
-}
-
-
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.my_eks_cluster.name
-  addon_name   = "vpc-cni"
-}
-
-
-resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.my_eks_cluster.name
-  addon_name   = "coredns"
-}
-
-
-resource "aws_eks_addon" "kube-proxy" {
-  cluster_name = aws_eks_cluster.my_eks_cluster.name
-  addon_name   = "kube-proxy"
-}
-
-
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-group-role"
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "noderole-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
+#resource "aws_iam_role" "eks_cluster_role" {
+#  name = "eks_cluster_role_terraform"
+#  assume_role_policy = <<POLICY
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Effect": "Allow",
+#      "Principal": {
+#        "Service": "eks.amazonaws.com"
+#      },
+#      "Action": "sts:AssumeRole"
+#    }
+#  ]
+#}
+#POLICY
+#}
+#
+#
+#resource "aws_iam_role_policy_attachment" "my-AmazonEKSClusterPolicy" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#  role       = aws_iam_role.eks_cluster_role.name
+#}
+#
+#
+#resource "aws_iam_role_policy_attachment" "my-AmazonEKSVPCResourceController" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+#  role       = aws_iam_role.eks_cluster_role.name
+#}
+#
+#
+#resource "aws_eks_cluster" "my_eks_cluster" {
+#  name     = var.eks_cluster_name
+#  role_arn = aws_iam_role.eks_cluster_role.arn
+#  vpc_config { 
+#    subnet_ids = [var.subnet_one, var.subnet_two]
+#  }
+#  depends_on = [
+#    aws_iam_role_policy_attachment.my-AmazonEKSClusterPolicy,
+#    aws_iam_role_policy_attachment.my-AmazonEKSVPCResourceController,
+#  ]
+#}
+#
+#
+#resource "aws_eks_addon" "vpc_cni" {
+#  cluster_name = aws_eks_cluster.my_eks_cluster.name
+#  addon_name   = "vpc-cni"
+#}
+#
+#
+#resource "aws_eks_addon" "coredns" {
+#  cluster_name = aws_eks_cluster.my_eks_cluster.name
+#  addon_name   = "coredns"
+#}
+#
+#
+#resource "aws_eks_addon" "kube-proxy" {
+#  cluster_name = aws_eks_cluster.my_eks_cluster.name
+#  addon_name   = "kube-proxy"
+#}
+#
+#
+#resource "aws_iam_role" "eks_node_role" {
+#  name = "eks-node-group-role"
+#  assume_role_policy = jsonencode({
+#    Statement = [{
+#      Action = "sts:AssumeRole"
+#      Effect = "Allow"
+#      Principal = {
+#        Service = "ec2.amazonaws.com"
+#      }
+#    }]
+#    Version = "2012-10-17"
+#  })
+#}
+#
+#resource "aws_iam_role_policy_attachment" "noderole-AmazonEKSWorkerNodePolicy" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#  role       = aws_iam_role.eks_node_role.name
+#}
 
 resource "aws_iam_role_policy_attachment" "noderole-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
