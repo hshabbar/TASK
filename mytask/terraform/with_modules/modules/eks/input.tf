4959759b61ea14ede754ecad9ebf08d6e48da3ff
hshabbar <91328430+hshabbar@users.noreply.github.com>
2024-09-21 10:15:39 +0530

    Update input.tf

--- mytask/terraform/with_modules/modules/eks/input.tf
+++ mytask/terraform/with_modules/modules/eks/input.tf
@@ -1,21 +1,21 @@
-variable "eks_cluster_name" {
-  description = "Name of EKS cluster"
-  type = string
-  default = "test-eks-cluster"
-}
-
-variable "nodegroup_name" {
-  description = "Name of EKS cluster's managed nodegroup"
-  type = string
-  default = "ng1"
-}
-
-variable "subnet_one" {
-  description = "ID of first subnet for cluster and worker nodes"
-  type = string
-}
-
-variable "subnet_two" {
-  description = "ID of second subnet for cluster and worker nodes"
-  type = string
-}
+#variable "eks_cluster_name" {
+#  description = "Name of EKS cluster"
+#  type = string
+#  default = "test-eks-cluster"
+#}
+#
+#variable "nodegroup_name" {
+#  description = "Name of EKS cluster's managed nodegroup"
+#  type = string
+#  default = "ng1"
+#}
+#
+#variable "subnet_one" {
+#  description = "ID of first subnet for cluster and worker nodes"
+#  type = string
+#}
+#
+#variable "subnet_two" {
+#  description = "ID of second subnet for cluster and worker nodes"
+#  type = string
+#}
