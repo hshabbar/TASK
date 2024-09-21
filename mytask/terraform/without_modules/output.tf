d0c768d06cbc760b64930c3e7146c90f5551d361
hshabbar <91328430+hshabbar@users.noreply.github.com>
2024-09-21 10:07:07 +0530

    Update output.tf

--- mytask/terraform/without_modules/output.tf
+++ mytask/terraform/without_modules/output.tf
@@ -27,6 +27,3 @@ output "private_subnet_two_id" {
 #  value = aws_eks_cluster.my_eks_cluster.endpoint
 #}
 #
-#output "kubeconfig-certificate-authority-data" {
-#  value = aws_eks_cluster.my_eks_cluster.certificate_authority[0].data
-#}
